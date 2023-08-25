45b7e0ee724a2dfbe03be4161eaf97acea388df1
VietDung7301 <vietdung.7301@gmail.com>
2023-08-25 08:49:35 +0700

    Django Tutorial Part 8: User authentication and permissions

--- catalog/admin.py
+++ catalog/admin.py
@@ -55,8 +55,7 @@ class BookInstanceAdmin(admin.ModelAdmin):
         list_filter: filters that will be displayed in sidebar
         fieldsets: groups of fields
     """
-    list_display = ('book', 'status', 'due_back', 'id')
-
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -64,6 +63,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
